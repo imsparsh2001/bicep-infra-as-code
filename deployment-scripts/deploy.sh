0c72c6ad9dec61d74507db570cfc2c4bafeceeed
imsparsh2001 <sparshjaipuriyar@gmail.com>
2025-05-07 02:25:21 +0530

    Tried using deployment script in biceps

---
+++ deployment-scripts/deploy.sh
@@ -0,0 +1,9 @@
+resourceGroupName="bicep-script-rg"  
+templateFile="main.bicep"
+today=$(date +"%d-%b-%Y")
+deploymentName="deploymentscript-"$today
+
+az deployment group create \
+    --resource-group $resourceGroupName \
+    --name $deploymentName \
+    --template-file $templateFile
